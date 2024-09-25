89ad441c0a949dd14a58821ee02cac94a686e6cf
Kyoku⭐ <144906395+KyokuKong@users.noreply.github.com>
2024-09-26 00:31:43 +0800

    修复Trace转换错误问题#2

--- logger_interface.go
+++ logger_interface.go
@@ -46,17 +46,15 @@ func (g *GormLogrusLogger) Trace(ctx context.Context, begin time.Time, fc func()
 	elapsed := time.Since(begin)
 	sql, rows := fc()
 	if err != nil {
-		g.log.WithContext(ctx).WithError(err).WithFields(logrus.Fields{
-			"rows":    rows,
-			"sql":     sql,
+		g.log.WithContext(ctx).WithFields(logrus.Fields{
 			"elapsed": elapsed,
-		}).Error("Gorm Trace")
+			"rows":    rows,
+		}).Errorf("SQL Error: %s", sql)
 	} else {
 		g.log.WithContext(ctx).WithFields(logrus.Fields{
-			"rows":    rows,
-			"sql":     sql,
 			"elapsed": elapsed,
-		}).Trace("Gorm Trace")
+			"rows":    rows,
+		}).Infof("Executed SQL: %s", sql) // 确保这里是 Info 而不是 Debug
 	}
 }
 
